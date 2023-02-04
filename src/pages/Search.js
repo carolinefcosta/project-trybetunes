bb89b336984b3a29c8754cea31bccd45835a5273
Caroline <carolinefernandesc@hotmail.com>
2023-02-04 07:59:45 -0300

    requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    artistName: '',
+    disabledButton: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.verifyButton();
+    });
+  };
+
+  verifyButton = () => {
+    const minLength = 2;
+    const { artistName } = this.state;
+    if (artistName.length >= minLength) {
+      this.setState({
+        disabledButton: false,
+      });
+    }
+  };
+
   render() {
+    const { artistName, disabledButton } = this.state;
     return (
       <>
         <div data-testid="page-search">
@@ -12,10 +37,22 @@ class Search extends Component {
 
           <label htmlFor="search-artist-input">
             <input
+              value={ artistName }
+              name="artistName"
+              type="text"
               id="search-artist-input"
               data-testid="search-artist-input"
+              onChange={ this.handleChange }
             />
           </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disabledButton }
+          >
+            Pesquisar
+
+          </button>
         </form>
       </>
     );
